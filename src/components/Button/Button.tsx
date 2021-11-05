3526c3359dcc4ebb59e1c74609d8566c4dc8fb20
orlovse <hajoff@gmail.com>
2021-11-05 20:47:07 +0200

    Add styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ import styles from './Button.css';
 
 export const Button: ButtonProps = ({ children, className, ...props }) => {
   return (
-    <button className={classNames(styles.button, className)} {...props}>
+    <button className={classNames(styles.glowOnHover, className)} {...props}>
       {children}
     </button>
   );
