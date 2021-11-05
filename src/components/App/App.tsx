014ca19eda2bbd9e4f45b7e7e893da41ccf22fb0
orlovse <hajoff@gmail.com>
2021-11-05 16:03:20 +0200

    Add styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,26 @@
-import Button from '../Button';
 import React, { FC } from 'react';
+import cn from 'classnames';
+
 import s from './App.css';
+
+import Button from '../Button';
+import { Loader } from '../Loader';
+import { Star } from '../Start';
+import { Input } from '../Input';
+import { Card } from '../Card';
+import { InputTypeEnum } from '../../commonTypes';
 
 export const App: FC = () => {
   return (
-    <div className={s.app}>
+    <div className={cn(s.app)}>
       <h1>Shortcut Battle</h1>
       <h2>Coming soon</h2>
       <br />
       <Button>Sample button</Button>
+      <Loader />
+      <Star />
+      <Input type={InputTypeEnum.password} />
+      <Card />
     </div>
   );
 };
