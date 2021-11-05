343d61e92497fd102f6e78755cee5fa652ee6ad7
orlovse <hajoff@gmail.com>
2021-11-05 15:35:26 +0200

    add svg loader to webpack

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,3 +2,7 @@ declare module '*.css' {
   const content: Record<string, string>;
   export default content;
 }
+declare module '*.svg' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
