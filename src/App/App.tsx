7a334a7ffcbe8a64dec69f87e77d8e35ac294a53
orlovse <hajoff@gmail.com>
2021-11-06 10:16:56 +0200

    Fix comments

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 
-import { Home } from '../../pages//Home';
-import { ComponentsLibrary } from '../../pages/ComponentsLibrary';
+import { Home } from '../pages/Home';
+import { ComponentsLibrary } from '../pages/ComponentsLibrary';
 
 import styles from './App.css';
 
@@ -14,12 +14,12 @@ export const App = () => {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/ComponentsLibrary">Components Library</Link>
+          <Link to="/library">Components Library</Link>
         </li>
       </ul>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/ComponentsLibrary" element={<ComponentsLibrary />} />
+        <Route path="/library" element={<ComponentsLibrary />} />
       </Routes>
     </div>
   );
