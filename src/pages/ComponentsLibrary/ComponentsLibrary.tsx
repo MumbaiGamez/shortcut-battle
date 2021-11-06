bc9cf15e64b2efb3b453db4c731cafc1e49f68ad
orlovse <hajoff@gmail.com>
2021-11-06 08:24:09 +0200

    Move components to page

--- src/pages/ComponentsLibrary/ComponentsLibrary.tsx
+++ src/pages/ComponentsLibrary/ComponentsLibrary.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import { Loader } from '../../components/Loader';
+import { Star } from '../../components/Start';
+import { Input } from '../../components/Input';
+import { Card } from '../../components/Card';
+import { Button } from '../../components/Button';
+
+import { InputTypeEnum } from '../../../typings/commonTypes';
+
 import styles from './ComponentsLibrary.css';
 
 export const ComponentsLibrary = () => {
-  return <div className={styles.container}>ComponentsLibrary page</div>;
+  return (
+    <div className={styles.container}>
+      <Loader />
+      <Star customClassName={styles.starWrapper} />
+      <Card>
+        <Input
+          type={InputTypeEnum.password}
+          validationRule={{ minSymbols: 6 }}
+        />
+        <Input validationRule={{ isRequired: true }} />
+        <Button>Sample button</Button>
+      </Card>
+    </div>
+  );
 };
