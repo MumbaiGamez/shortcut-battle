a9c2d4fab9f95d07e89959cfa804ceb4212f62e4
orlovse <hajoff@gmail.com>
2021-11-06 10:56:32 +0200

    Fix styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../../pages//Home';
 import { ComponentsLibrary } from '../../pages/ComponentsLibrary';
+
+import { NavigationMenu } from '../NavigationMenu';
 
 import styles from './App.css';
 
 export const App = () => {
   return (
     <div className={styles.app}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/library">Components Library</Link>
-        </li>
-      </ul>
+      <NavigationMenu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/library" element={<ComponentsLibrary />} />
