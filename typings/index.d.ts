474c9b834fd4b4619ba3a478ec1fdec66c2e8310
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-11-07 14:57:31 +0200

    Add player texture

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,3 +6,8 @@ declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,10 @@ module.exports = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
       },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
