df19e7826a354fefc800bea0b152f3958a87acc2
orlovse <hajoff@gmail.com>
2021-11-07 12:25:30 +0200

    Add isValid to getValidationError

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -5,20 +5,26 @@ export const getValidationError = (
   rules: ValidationRules | undefined,
   value: string | number
 ) => {
+  let isValid = true;
+
   if (!rules) {
-    return '';
+    return { isValid, errorMessage: '' };
   }
 
   let errorMessage = '';
   if (rules.isRequired && !value) {
+    isValid = false;
     errorMessage = `Field is required`;
   } else if (rules.minSymbols && rules.minSymbols > value.toString().length) {
+    isValid = false;
     errorMessage = `Less then ${rules.minSymbols} symbols`;
   } else if (rules.phone && !value.toString().match(PHONE_REG_EXP)) {
+    isValid = false;
     errorMessage = `Invalid phone`;
   } else if (rules.email && !value.toString().match(EMAIL_REG_EXP)) {
+    isValid = false;
     errorMessage = `Invalid email`;
   }
 
-  return errorMessage;
+  return { isValid, errorMessage };
 };
