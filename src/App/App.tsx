1c10abe2df9248167749277ed45f26f9f639c84f
Sergey Orlov <54174889+orlovse@users.noreply.github.com>
2021-11-07 12:46:05 +0200

    Add react router (#8)
    ## Changes
    * Install react-router-dom
    * Add empty pages
    * Add basic routs
    * Fix comments

---
+++ src/App/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import { Home } from '../pages/Home';
+import { ComponentsLibrary } from '../pages/ComponentsLibrary';
+
+import styles from './App.css';
+
+export const App = () => {
+  return (
+    <div className={styles.app}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/library">Components Library</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/library" element={<ComponentsLibrary />} />
+      </Routes>
+    </div>
+  );
+};
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './Home.css';
+
+export const Home = () => {
+  return <div className={styles.container}>Home page</div>;
+};
