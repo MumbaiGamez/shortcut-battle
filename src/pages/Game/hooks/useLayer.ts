119eb62c8b578895f53ada1878569f512abf9cf3
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-11-10 00:08:16 +0200

    Simplify boolean expression

--- src/pages/Game/hooks/useLayer.ts
+++ src/pages/Game/hooks/useLayer.ts
@@ -8,11 +8,8 @@ const haveCollisions = (boundaries1: number[], boundaries2: number[]) => {
   const [left1, top1, right1, bottom1] = boundaries1;
   const [left2, top2, right2, bottom2] = boundaries2;
 
-  return !(
-    right1 < left2 ||
-    left1 > right2 ||
-    bottom1 < top2 ||
-    top1 > bottom2
+  return (
+    right1 >= left2 && left1 <= right2 && bottom1 >= top2 && top1 <= bottom2
   );
 };
 
