cb0ed7bd440c97a1bf0dc30b30514a021ee30355
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2021-12-05 00:04:14 +0200

    Remove bus provider

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -4,7 +4,6 @@ import classNames from 'classnames';
 import { GameUI } from './components/GameUI';
 import { Playground } from './components/Playground';
 import { useFullscreen } from './hooks/useFullscreen';
-import { Provider as EventBusProvider } from './hooks/useBus';
 
 import styles from './Game.css';
 
@@ -14,17 +13,12 @@ export const Game = () => {
   const { isFullscreen, toggleFullscreen } = useFullscreen(pageRef.current);
 
   return (
-    <EventBusProvider>
-      <main
-        className={classNames(styles.game, isFullscreen && styles.fullscreen)}
-        ref={pageRef}
-      >
-        <GameUI
-          isFullscreen={isFullscreen}
-          toggleFullscreen={toggleFullscreen}
-        />
-        <Playground />
-      </main>
-    </EventBusProvider>
+    <main
+      className={classNames(styles.game, isFullscreen && styles.fullscreen)}
+      ref={pageRef}
+    >
+      <GameUI isFullscreen={isFullscreen} toggleFullscreen={toggleFullscreen} />
+      <Playground />
+    </main>
   );
 };
