e6c10a652948cc1048120246d000a24894b8a0c9
orlovse <hajoff@gmail.com>
2022-01-22 22:00:26 +0200

    Change Avatar component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -7,12 +7,18 @@ import { AvatarProps } from './types';
 import styles from './Avatar.css';
 
 export const Avatar = (props: AvatarProps) => {
-  const { handleChangeAvatar, handleDeleteAvatar, name, src } = props;
+  const {
+    handleChangeAvatar,
+    handleDeleteAvatar,
+    name,
+    src,
+    size = 100,
+  } = props;
 
   const initials = name?.[0]?.toUpperCase();
 
   return (
-    <div className={styles.avatar}>
+    <div className={styles.avatar} style={{ minWidth: size, height: size }}>
       {handleChangeAvatar && (
         <FilePicker
           containerClassName={styles.input}
@@ -25,7 +31,11 @@ export const Avatar = (props: AvatarProps) => {
           <div className={styles.delete} onClick={handleDeleteAvatar} />
         </>
       )}
-      {initials && <span className={styles.initials}>{initials}</span>}
+      {initials && (
+        <span className={styles.initials} style={{ fontSize: size / 2 }}>
+          {initials}
+        </span>
+      )}
     </div>
   );
 };
