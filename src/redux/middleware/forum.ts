aca369e271b69a3be0fe55b72b60121aba4d6184
orlovse <hajoff@gmail.com>
2022-01-27 18:40:07 +0200

    Remove console log

--- src/redux/middleware/forum.ts
+++ src/redux/middleware/forum.ts
@@ -4,7 +4,6 @@ import { endpoints as forumEndpoints } from '../api/forumApi';
 
 export const forumMiddleware: Middleware = () => (next) => (action) => {
   if (forumEndpoints.addTopic.matchFulfilled(action)) {
-    console.log('tut???');
     // @ts-ignore
     api.dispatch(forumEndpoints.getTopics.initiate());
   }
