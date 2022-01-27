506d2ddeef79d994cea88a08dc1fb5b59148322a
orlovse <hajoff@gmail.com>
2022-01-27 18:51:31 +0200

    Fix forumMiddleware

--- src/redux/middleware/forum.ts
+++ src/redux/middleware/forum.ts
@@ -1,12 +1,13 @@
-import { Middleware } from '@reduxjs/toolkit';
+import { Middleware, MiddlewareAPI } from '@reduxjs/toolkit';
 
 import { endpoints as forumEndpoints } from '../api/forumApi';
 
-export const forumMiddleware: Middleware = () => (next) => (action) => {
-  if (forumEndpoints.addTopic.matchFulfilled(action)) {
-    // @ts-ignore
-    api.dispatch(forumEndpoints.getTopics.initiate());
-  }
+export const forumMiddleware: Middleware =
+  (api: MiddlewareAPI) => (next) => (action) => {
+    if (forumEndpoints.addTopic.matchFulfilled(action)) {
+      // @ts-ignore
+      api.dispatch(forumEndpoints.getTopics.initiate());
+    }
 
-  return next(action);
-};
+    return next(action);
+  };
